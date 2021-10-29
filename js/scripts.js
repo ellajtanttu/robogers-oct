16fa1375e4191290eaf6a338a946e80f8b05319b
Ella Tanttu <ellajtanttu@gmail.com>
2021-10-29 09:31:40 -0700

    passed test: return is  [0, 'Beep!', 2, 'Won't you be my neighbor?', 4]

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 // Business Logic:
 function beepBoop(number) {
-  const inputNumber = number
+  const inputNumber = number;
   let inputRange = [];
   let outputRange = [];
   for (let i = 0; i < inputNumber + 1; i++) {
     inputRange.push(i);
   }
   inputRange.forEach(function(element) {
-    if (element === 1) {
-      outputRange.push("Beep!")
+    if (element === 3) {
+      outputRange.push("Won't you be my neighbor?");
+    } else if (element === 1) {
+      outputRange.push("Beep!");
     } else {
-      outputRange.push(element)
+      outputRange.push(element);
     }
   });
   console.log(outputRange);

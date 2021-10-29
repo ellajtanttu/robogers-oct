3bc2e82d239215230e23b6b7bbcbd9350dc1187e
Ella Tanttu <ellajtanttu@gmail.com>
2021-10-29 09:11:11 -0700

    passed test: return is [0,1,2,3,4]

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 // Business Logic:
 function beepBoop(number) {
-  return number;
+  let inputRange = [];
+  const inputNumber = number
+  for (let i = 0; i < inputNumber + 1; i++) {
+    inputRange.push(i);
+  }
+  console.log(inputRange);
+  return inputRange;
 };
 
 // UI Logic:
